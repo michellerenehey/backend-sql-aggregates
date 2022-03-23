ad8d2e9268f8e2a4ffc06deb770ee2909721a597
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-23 12:19:18 -0700

    exercise 1 passing

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,22 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT
+    film.title,
+    COUNT(*) 
+FROM
+	film
+INNER JOIN
+	inventory
+ON
+	film.film_id = inventory.film_id
+INNER JOIN
+	rental
+ON
+	rental.inventory_id = inventory.inventory_id
+GROUP BY
+	film.film_id 
+ORDER BY
+	count DESC
+LIMIT
+	10;
