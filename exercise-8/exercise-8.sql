7c755b1b2ce43e747c065afb12e37c005da4efe2
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-23 13:10:17 -0700

    exercise 9 passing

--- exercise-8/exercise-8.sql
+++ exercise-8/exercise-8.sql
@@ -1,2 +1,27 @@
 -- rank of film category by times rented
 -- include the name, count, and rank
+
+SELECT
+	name, 
+    COUNT(rental_id),
+    RANK() OVER (ORDER BY COUNT(rental_id) DESC)
+FROM
+	category
+INNER JOIN
+	film_category
+ON
+	category.category_id = film_category.category_id
+INNER JOIN
+	film
+ON
+	film_category.film_id = film.film_id
+INNER JOIN
+	inventory
+ON
+	film.film_id = inventory.film_id
+INNER JOIN
+	rental
+ON
+	inventory.inventory_id = rental.inventory_id
+GROUP BY
+	name;
