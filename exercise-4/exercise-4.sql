68129b5b7450d429135b1d3906940bad285164d4
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-23 12:47:25 -0700

    exercise 4 passing

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,18 @@
 -- the average film length by category
 -- include the category name and avg length
+
+SELECT
+	category.name,
+	avg(length)
+FROM
+	category
+INNER JOIN
+	film_category
+ON
+	category.category_id = film_category.category_id
+INNER JOIN
+	film
+ON
+	film_category.film_id = film.film_id
+GROUP BY
+    category.name;
